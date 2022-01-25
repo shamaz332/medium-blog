0cd28eff13437cb13cae9b1f4b8c7b47c4223904
Shamaz saeed <shamazsaeed332@gmail.com>
2022-01-25 21:44:39 +0500

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,12 +3,26 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header>
-      <div>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
         <Link href="/">
-          <img className="w-44 object-contain cursor-pointer" src="https://links.papareact.com/yvf" />
+          <img
+            className="w-44 cursor-pointer object-contain"
+            src="https://links.papareact.com/yvf"
+          />
         </Link>
-      </div>
+        <div className="hidden items-center space-x-5 md:inline-flex">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
+            Follow
+          </h3>
+        </div>   </div>
+        <div className="flex items-center space-x-5 text-green-600">
+          <h3>Sign In</h3>
+          <h3 className="border px-4 py-1 rounded-full border-green-600">Get Started</h3>
+        </div>
+   
     </header>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="py-2">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
