7a0ad483cd3dc10b906b1c93136416347cc2dc59
Shamaz saeed <shamazsaeed332@gmail.com>
2022-05-19 01:29:33 +0500

    post fetched and show

--- mediumclone/schemas/post.js
+++ mediumclone/schemas/post.js
@@ -7,6 +7,12 @@ export default {
       name: 'title',
       title: 'Title',
       type: 'string',
+
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
     },
     {
       name: 'slug',
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,32 @@
+import { sanityClient, urlFor } from '../sanity'
+
 import Head from 'next/head'
 import Header from '../components/Header'
+import Link from 'next/link'
+import { Post } from '../typings'
 
-export default function Home() {
+interface Props {
+  posts: [Post]
+}
+
+export default function Home({ posts }: Props) {
+  console.log(posts)
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="mx-auto max-w-7xl">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
-      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
-        <div className="px-10 space-y-5 ">
-          <h1 className="text-6xl max-w-xl font-serif "><span className="underline docoration-black docoration-4">Medium</span> is a place to read write and connect</h1>
+      <div className="flex items-center justify-between border-y border-black bg-yellow-400 py-10 lg:py-0">
+        <div className="space-y-5 px-10 ">
+          <h1 className="max-w-xl font-serif text-6xl ">
+            <span className="docoration-black docoration-4 underline">
+              Medium
+            </span>{' '}
+            is a place to read write and connect
+          </h1>
           <h2>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex soluta
             asperiores incidunt similique pariatur facilis nihil quod repellat,
@@ -20,8 +34,60 @@ export default function Home() {
             quisquam delectus.
           </h2>
         </div>
-      <img className="hidden md:inline-flex h-32 lg:h-full" src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="medium"/>
+        <img
+          className="hidden h-32 md:inline-flex lg:h-full"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+          alt="medium"
+        />
+      </div>
+
+      {/* posts */}
+
+      <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post.${post.slug.current}`}>
+            <div className="group cursor-pointer border rounded-lg overflow-hidden"> 
+              <img className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out" src={urlFor(post.mainImage).url()!} alt="main image" />
+              <div  className="flex justify-between bg-white p-5">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <div>
+                  <img
+                    className="h-12 w-12 rounded-full"
+                    src={urlFor(post.author.image).url()!}
+                    alt="author image"
+                  />
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = `*[_type=="post"]{
+ 
+    _id ,
+    title,
+    description,
+    mainImage,
+    slug,
+    author ->{
+    name,
+    image
+  }  
+  }`
+  const posts = await sanityClient.fetch(query)
+  return {
+    props: {
+      posts: posts,
+    },
+  }
 }
